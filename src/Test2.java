6b8c64ebcd698ac37972fee170da2f9b0080be74
Soniagith <soniaakter015@gmail.com>
2023-02-05 20:18:42 -0500

    Added additional check

--- src/Test2.java
+++ src/Test2.java
@@ -3,7 +3,7 @@ public class Test2 {
 
 	public static void main(String[] args) {
 		String hungry="yes";
-		if(hungry=="yes") {
+		if(hungry=="yes"||hungry=="y") {
 			System.out.println("Eat someting....");
 		}
 		else {
